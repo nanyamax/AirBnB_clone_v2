7d438f39bc8948c1113937f3f578fdc577e9259b
nanyamax <ifunanyasunday7@gmail.com>
2023-03-23 01:49:18 +0200

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -21,16 +21,27 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if kwargs:
-            if kwargs.get('__class__', None):
-                del kwargs['__class__']
-            for k, v in kwargs.items():
-                if k == 'created_at' or k == 'updated_at':
-                    v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
-                self.__dict__.update({k: v})
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            try:
+                time_format = "%Y-%m-%dT%H:%M:%S.%f"
+                kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], time_format
+                )
+                kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"], time_format
+                )
+            except KeyError:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                setattr(self, key, value)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
