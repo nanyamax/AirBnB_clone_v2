410afb6367235c2966a045d6ae5f9848d42f36e7
nanyamax <ifunanyasunday7@gmail.com>
2023-03-23 02:10:33 +0200

    updated

--- models/user.py
+++ models/user.py
@@ -3,7 +3,6 @@
 """
 A module that defines the ORM class for User table
 """
-from os import getenv
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -15,17 +14,11 @@ class User(BaseModel, Base):
     """
     __tablename__ = 'users'
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship(
-            'Place', backref='user', cascade='all, delete')
-        reviews = relationship(
-            'Review', backref='user', cascade='all, delete')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship(
+            'Place', backref='user', cascade='all, delete, delete-orphan')
+    reviews = relationship(
+            'Review', backref='user', cascade='all, delete, delete-orphan')
