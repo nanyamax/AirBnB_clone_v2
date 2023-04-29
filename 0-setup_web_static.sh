006e3586f89110ed47afd2b30842fbc4162a8705
nanyamax <ifunanyasunday7@gmail.com>
2023-04-29 12:48:04 +0000

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
-
 # Install Nginx if not already installed
-apt-get update
-apt-get -y install nginx
+sudo apt-get update
+sudo apt-get -y install nginx
 
 # Create necessary directories and files for web_static
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo '<html><head></head><body>Holberton School</body></html>' > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
-
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo echo '<html><head></head><body>Holberton School</body></html>' > /data/web_static/releases/test/index.html
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 # Update Nginx config to serve web_static files
-sed -i '51 i \\\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '51 i \\\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 service nginx restart
