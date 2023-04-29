06fca68dd3ee1b42f483bed9c26daf427f9267b2
nanyamax <ifunanyasunday7@gmail.com>
2023-04-29 15:31:15 +0000

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ sudo apt-get -y install nginx
 # Create necessary directories and files for web_static
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-echo '<html><head></head><body>Holberton School</body></html>'| sudo tee -a < /data/web_static/releases/test/index.html>
+echo '<html><head></head><body>Holberton School</body></html>' > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 # Update Nginx config to serve web_static files
